335dc7379243ee687cda1f57779aa296c91e87eb
Monalisa222 <monalisadengre222@gmail.com>
2026-05-06 07:34:18 +0000

    Add failing specs for employee salary numericality validation

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -10,6 +10,23 @@ RSpec.describe Employee, type: :model do
     it { should validate_presence_of(:salary) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
+
+    it do
+      should validate_numericality_of(:salary)
+        .is_greater_than(0)
+    end
+
+    it "is invalid with a negative salary" do
+      employee = build(:employee, salary: -1000)
+      expect(employee).not_to be_valid
+      expect(employee.errors[:salary]).to include("must be greater than 0")
+    end
+
+    it "is invalid with a zero salary" do
+      employee = build(:employee, salary: 0)
+      expect(employee).not_to be_valid
+      expect(employee.errors[:salary]).to include("must be greater than 0")
+    end
   end
 
   describe "callbacks" do
