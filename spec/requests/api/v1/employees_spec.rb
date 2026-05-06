c271bf479651beea88d06be53582b92b14034150
Monalisa222 <monalisadengre222@gmail.com>
2026-05-06 07:50:40 +0000

    Add failing spec for employee creation validation errors

--- spec/requests/api/v1/employees_spec.rb
+++ spec/requests/api/v1/employees_spec.rb
@@ -19,5 +19,28 @@ RSpec.describe "Api::V1::Employees", type: :request do
 
       expect(response).to have_http_status(:created)
     end
+
+    it "returns errors for invalid attributes" do
+      invalid_params = {
+        employee: {
+          full_name: "",
+          job_title: "",
+          country: "",
+          salary: -1000,
+          email: "invalid-email"
+        } 
+      }
+
+      post "/api/v1/employees", params: invalid_params
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      body = JSON.parse(response.body)
+
+      expect(body["errors"]).to include("Full name can't be blank")
+      expect(body["errors"]).to include("Job title can't be blank")
+      expect(body["errors"]).to include("Country can't be blank")
+      expect(body["errors"]).to include("Salary must be greater than 0")
+      expect(body["errors"]).to include("Email is invalid")
+    end
   end
 end
