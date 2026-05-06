3b009f91ef2a1cb4402b15f1029af3b59c97e9aa
Monalisa222 <monalisadengre222@gmail.com>
2026-05-06 07:16:21 +0000

    Refactor employee validation spec using shoulda matchers

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
   describe "validations" do
-    it "is invalid without a full_name" do
-      employee = build(:employee, full_name: nil)
-      expect(employee).not_to be_valid
-      expect(employee.errors[:full_name]).to include("can't be blank")
-    end
+    it { should validate_presence_of(:full_name) }
   end
 end
