efc2f601f816455d2dbc58e14e5ac3b0489602df
Monalisa222 <monalisadengre222@gmail.com>
2026-05-06 07:35:09 +0000

    Refine employee salary validation to require positive values

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,7 @@ class Employee < ApplicationRecord
   validates :full_name, presence: true
   validates :job_title, presence: true
   validates :country, presence: true
-  validates :salary, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :salary, presence: true, numericality: { greater_than: 0 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   before_save :normalize_email
